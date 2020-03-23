9fe7ce89a4e3c40b34bfdcdb86c18f6a21573244
pvermees <p.vermeesch@ucl.ac.uk>
2020-03-23 19:35:22 +0000

    Redefined M2m and m2M

--- semitracks-test.R
+++ semitracks-test.R
@@ -1,9 +1,9 @@
 rm(list=ls())
 graphics.off()
 setwd('~/Documents/Programming/R/fissiontracks/')
-source('semitracks.R')
+source('semitracks2.R')
 
-example <- 4
+example <- 2
 
 if (example==1){
     fname <- 'track-lengths/fct_c_semi.csv'
@@ -29,44 +29,82 @@ if (example==1){
 
 dat <- read.data(fname,confined=confined,skip=skip,cols=cols)
 
-option <- 2
+option <- 10
 
-if (option==1) {
-    P <- c(0.3,0.7)
-    M <- c(8,13)
-    S <- 1
-    fm <- forward(P,M,S)
-    plotModel(fm)
-    l <- simulate(fm,n=1000,confined=FALSE)
-    fit <- invert(l,confined=FALSE,ncomp=2)
-} else if (option==2){
-    fit <- invert(dat$l,confined=confined,ncomp=1)
-    fm <- forward(fit$P,fit$M,fit$S)
-    plotModel(fm,dat)
-} else if (option==3){ # plot fs against l for different phi
-    nl <- 50
-    np <- 10
-    l <- seq(from=0,to=15,length.out=nl)
-    phi <- seq(from=pi/2,to=0,length.out=np)
-    M <- 10
-    S <- 1
-    for (i in 1:np){
-        dat <- cbind(l,phi[i])
-        colnames(dat) <- c('length','angle')
-        f <- getfsLA(dat,M,S)
-        if (i==1) plot(l,f,type='l')
-        else lines(l,f,type='l')
+if (option==10){
+    nc <- 5
+    nr <- 50
+    M <- seq(from=5,to=16,length.out=nc)
+    S <- seq(from=0.2,to=2,length.out=nc)
+    l <- seq(from=0,to=16,length.out=nr)
+    phi <- rep(45*pi/180,nr)
+    dat <- cbind(l,phi)
+    colnames(dat) <- c('length','angle')
+    lf <- matrix(0,nr,nc)
+    x <- lf
+    for (i in 1:nc){
+        x[,i] <- l
+        lf[,i] <- log(getfs_l_phi(dat,M=10,S=S[i]))
     }
-} else if (option==4){ # plot E[l,mu] on angle plot
-    nm <- 3
-    M <- seq(from=15,to=7,length.out=nm)
-    np <- 100
-    phi <- seq(from=0,to=pi/2,length.out=np)
-    plot(c(0,20),c(0,100),type='n',xlab='length',ylab='angle')
-    for (i in 1:nm){
-        l <- getElphi(mu=M[i],phi)
-        lines(l,phi*180/pi,type='l')
+    matplot(x=l,y=lf,type='l')
+} else if (option==9){
+    fs <- log(getfs_l_phi(dat,M=6,S=10))
+    mfs <- min(fs)
+    Mfs <- max(fs)
+    r <- (fs-mfs)/(Mfs-mfs)
+    g <- (Mfs-fs)/(Mfs-mfs)
+    b <- 0
+    plot(dat[,'length'],dat[,'angle'],pch=19,col=rgb(cbind(r,g,b)))
+    title(main=c(mfs,Mfs,sum(fs)))
+} else if (option==8){
+    nn <- 50
+    M <- seq(from=5,to=16,length.out=nn)
+    S <- seq(from=0.2,to=2,length.out=nn)
+    mf <- matrix(0,nn,nn)
+    for (i in 1:nn){
+        m <- M2m(M[i])
+        for (j in 1:nn){
+            s <- S2s(S[i])
+            mf[i,j] <- sum(log(getfs_l_phi(dat,M=M[i],S=S[j])))
+            #mf[i,j] <- smisfit(c(m,s),dat)
+        }
     }
+    contour(x=M,y=S,z=mf)
+} else if (option==7){
+    nn <- 100
+    l <- seq(from=0,to=16,length.out=nn)
+    a <- seq(from=0,to=pi/2,length.out=nn)
+    lf <- matrix(0,nn,nn)
+    gdat <- matrix(0,1,2)
+    colnames(gdat) <- c('length','angle')
+    P1 <- 1
+    P2 <- 0.7
+    M1 <- 10
+    M2 <- 5
+    S <- 2
+    for (i in 1:nn){
+        gdat[1,'length'] <- l[i]
+        for (j in 1:nn){
+            gdat[1,'angle'] <- a[j]
+            lf[i,j] <- log(P1*getfs_l_phi(gdat,M=M1,S=S)) # +
+            # log(P2 * getfs_l_phi(gdat,M=M2,S=S))
+        }
+    }
+    pdat <- dat
+    pdat[,'angle']<- dat[,'angle']*180/pi
+    plot(pdat,pch=21,col='grey')
+    contour(x=l,y=a*180/pi,z=lf,
+            xlab='length',ylab='angle',add=TRUE)
+    p <- 0#P2p(c(P1,P2))
+    m <- M2m(M1)#,M2m(c(M1,M2),mM=5,MM=16)
+    s <- S2s(S)
+    mf <- smisfit(c(m,s),dat,mM=5,MM=16)
+    title(main=mf)
+} else if (option==6){
+    f <- log(getfs_l_phi(dat=dat,M=10,S=1))
+    plot(dat[,'angle'],f,type='p')
+    title(main=sum(f))
+    fit <- invert(dat,confined=confined,ncomp=1,mS=1,MS=1)
 } else if (option==5){ # plot fs against l for different M
     nl <- 50
     nm <- 3
@@ -81,8 +119,42 @@ if (option==1) {
         if (i==1) plot(l,f,type='l')
         else lines(l,f,type='l')
     }
-} else if (option==6){
-    fit <- optim(log(c(10,1)),smisfit,dat=dat)
+} else if (option==4){ # plot E[l,mu] on angle plot
+    nm <- 3
+    M <- seq(from=15,to=7,length.out=nm)
+    np <- 100
+    phi <- seq(from=0,to=pi/2,length.out=np)
+    plot(c(0,20),c(0,100),type='n',xlab='length',ylab='angle')
+    for (i in 1:nm){
+        l <- getElphi(mu=M[i],phi)
+        lines(l,phi*180/pi,type='l')
+    }
+} else if (option==3){ # plot fs against l for different phi
+    nl <- 50
+    np <- 10
+    l <- seq(from=0,to=15,length.out=nl)
+    phi <- seq(from=pi/2,to=0,length.out=np)
+    M <- 10
+    S <- 1
+    for (i in 1:np){
+        dat <- cbind(l,phi[i])
+        colnames(dat) <- c('length','angle')
+        f <- getfsLA(dat,M,S)
+        if (i==1) plot(l,f,type='l')
+        else lines(l,f,type='l')
+    }
+} else if (option==2){
+    fit <- invert(dat$l,confined=confined,ncomp=1)
+    fm <- forward(fit$P,fit$M,fit$S)
+    plotModel(fm,dat)
+} else if (option==1) {
+    P <- c(0.3,0.7)
+    M <- c(8,13)
+    S <- 1
+    fm <- forward(P,M,S)
+    plotModel(fm)
+    l <- simulate(fm,n=1000,confined=FALSE)
+    fit <- invert(l,confined=FALSE,ncomp=2)
 }
 
 #mod <- forward(fit$P,fit$M,fit$S)
--- semitracks2.R
+++ semitracks2.R
@@ -1,5 +1,6 @@
 # fname: the name of a .csv file containing the length measurements
-# confined = FALSE if fname contains semi-tracks, = TRUE if it contains confined tracks
+# confined = FALSE if fname contains semi-tracks,
+#          = TRUE if it contains confined tracks
 # header: does the .csv file contain a header?
 # skip: how many lines does the header contain
 # cols: column numbers of the true lengths and angles to the c-axis
@@ -22,75 +23,67 @@ invert <- function(dat,confined=FALSE,ncomp=2,mM=5,MM=16){
     pms <- rep(0,2*ncomp)
     if (confined) fit <- optim(pms,cmisfit,dat=dat)
     else fit <- optim(pms,smisfit,dat=dat,mM=mM,MM=MM)
-    if (ncomp>1) P <- p2P(fit$par[1:(ncomp-1)])
-    else P <- 1
-    M <- m2M(fit$par[ncomp:(2*ncomp-1)])
-    S <- s2S(fit$par[2*ncomp])
+    if (ncomp==1) P <- 1
+    else P <- p2P(fit$par[1:(ncomp-1)])
+    m <- fit$par[ncomp:(2*ncomp-1)]
+    M <- m2M(m,mM=mM,MM=MM)
+    s <- fit$par[2*ncomp]
+    S <- s2S(s)
     list(P=P,M=M,S=S)
 }
 
 # maximum (negative) likelihood misfit function for semi-tracks
 # ^f
 # |----------
-# |      |   \
-# |      |    \
-# |      |     \
-# |      |      ----\
-# |      |           \
-# |      |            \
-# ------mM------------MM---------> l
-smisfit <- function(pms,dat,mM=4,MM=16){
+# |      |   \            |
+# |      |    \           |
+# |      |     \          |
+# |      |      ----\     |
+# |      |           \    |
+# |      |            \   |
+# ------mM----------------MM-----> l
+smisfit <- function(pms,dat,mM=5,MM=16,mS=0.2,MS=2){
     ncomp <- length(pms)/2
-    if (ncomp==1){
-        P <- 1
-    } else {
-        P <- p2P(pms[1:(ncomp-1)])
-    }
+    if (ncomp==1) P <- 1
+    else P <- p2P(pms[1:(ncomp-1)])
     m <- pms[ncomp:(2*ncomp-1)]
     M <- m2M(m,mM=mM,MM=MM)
     s <- pms[2*ncomp]
-    S <- s2S(s)
+    S <- s2S(s,mS=mS,MS=MS)
     out <- 0
     for (i in 1:ncomp){
-        f <- getfsLA(dat=dat,M=M[i],S=S)
-        out <- out + sum(log(P[i]*f))
+        f <- getfs_l_phi(dat=dat,M=M[i],S=S)
+        out <- out - sum(log(P[i]*f))
     }
-    print(c(M,S,out))
-    -out
-}
-smisfit.temp <- function(ms,dat){
-    M <- exp(ms[1])
-    S <- exp(ms[2])
-    f <- getfsLA(dat=dat,M=M,S=S)
-    -sum(log(f))
+    out
 }
 
-getfsLA <- function(dat,M,S){
+getfs_l_phi <- function(dat,M,S){
     l <- dat[,'length']
     phi <- dat[,'angle']
-    N <- getN(M)
-    E <- getE(M)
-    Ms <- N+4*E/(3*pi)
-    (1-pnorm(q=l,mean=N+E*cos(phi),sd=S))*(sin(phi)^2)*4/(pi*Ms)
+    eta <- getEta(M)
+    xi <- getXi(M)
+    Ms <- eta+4*xi/(3*pi)
+    (1-pnorm(q=l,mean=eta+xi*cos(phi),sd=S))*(sin(phi)^2)*4/(pi*Ms)
 }
 
 # equation 7.53
-getN <- function(mu){
+getEta <- function(mu){
     -5.18 + 1.42*mu - sqrt(11.48 - 2.13*mu + 0.1*mu^2)
 }
 
 # equation 7.53
-getE <- function(mu){
-    2*(mu-getN(mu))
+getXi <- function(mu){
+    2*(mu-getEta(mu))
 }
 
 # middle of page 140
 getMs <- function(mu){
-    getN(mu)+4*getE(mu)/(3*pi)
+    getEta(mu)+4*getXi(mu)/(3*pi)
 }
 
-getElphi <- function(mu,phi){
-    getN(mu) + getE(mu)*cos(phi)
+getELA <- function(mu,phi){
+    getEta(mu) + getXi(mu)*cos(phi)
 }
 
 # m = forward model, d = data
@@ -113,7 +106,7 @@ plotModel <- function(m,d=NULL){
             plot(m$l,m$fs,type='l',xlab=xlab)
         } else {
             cutoff <- min(d$l)
-            fsL.of.uncounted <- getfsL(cutoff,P=m$P,M=m$M,S=m$S)
+            fsL.of.uncounted <- getfs_l(cutoff,P=m$P,M=m$M,S=m$S)
             corr.factor <- 1/(1-cutoff*fsL.of.uncounted)
             lines(m$l,m$fs*corr.factor)
             title(smisfit(m$pms,d$l))
@@ -126,14 +119,14 @@ plotModel <- function(m,d=NULL){
 # M = vector with the means of the length peaks
 # S = vector with the standard deviations of the length peaks
 # P, M and S must have the same lengths
-forward <- function(P,M,S,mM=4,MM=16) {
+forward <- function(P,M,S,mM=5,MM=16) {
     # calculate the confined and semi-track length distributions
     l <- seq(from=0,to=20,by=0.01) # confined fission track lengths to evaluate
     fc <- l
     fs <- l
     for (i in 1:length(l)){       # loop through all the lengths
-        fc[i] <- getfcL(l[i],P,M,S)  # calculate the pdf of the confined track lengths
-        fs[i] <- getfsL(l[i],P,M,S)  # calculate the pdf of the semi-track lengths
+        fc[i] <- getfc_l(l[i],P,M,S)  # calculate the pdf of the confined track lengths
+        fs[i] <- getfs_l(l[i],P,M,S)  # calculate the pdf of the semi-track lengths
     }
     pms <- c(P2p(P),M2m(M,mM,MM),S2s(S))
     list(l=l,fs=fs,fc=fc,P=P,M=M,S=S,pms=pms)
@@ -142,13 +135,15 @@ forward <- function(P,M,S,mM=4,MM=16) {
 ## log(ratio) transformations:
 P2p <- function(P){
     nn <- length(P)
-    log(P[1:(nn-1)])-log(P[nn])
+    log(P[1:(nn-1)]) - log(P[nn])
 }
-M2m <- function(M,mM=4,MM=16){
-    log(diff(c(mM,M,MM)))
+M2m <- function(M,mM=5,MM=16){
+    nM <- length(M)
+    dM <- diff(c(mM,M))
+    log(dM) - log(MM-M[nM])
 }
-S2s <- function(S){
-    log(S)
+S2s <- function(S,mS=0.2,MS=2){
+    log(S-mS) - log(MS-S)
 }
 
 ## inverse log(ratio) transformations:
@@ -157,11 +152,10 @@ p2P <- function(p){
 }
 # m = log of the ratios of the differences between the peak locations
 # mM, MM = minimum and maximum track length
-m2M <- function(m,mM=4,MM=16){
-    nn <- length(m)
-    dM <- c(exp(m),1)/(sum(exp(m))+1)
-    mM + (MM-mM)*cumsum(dM)[1:nn]
+m2M <- function(m,mM=5,MM=16){
+    dM <- (MM-mM)*exp(m)/(1+sum(exp(m)))
+    mM + cumsum(dM)
 }
-s2S <- function(s){
-    exp(s)
+s2S <- function(s,mS=0.2,MS=2){
+    mS + (MS-mS)*exp(s)/(1+exp(s))
 }
