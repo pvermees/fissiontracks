48a59d13ba719e969b5b3ce16b8d6a5cf03cd4b1
pvermees <p.vermeesch@ucl.ac.uk>
2025-12-02 18:04:19 +0000

    Updated MiA2GaH.R

--- geochron@home/MiA2GaH.R
+++ geochron@home/MiA2GaH.R
@@ -3,16 +3,27 @@ options(warn=2)
 MiA2GaH <- function(idir,odir,IMpath=NULL){
     grains <- list.files(idir,pattern="Grain*")
     convert <- ifelse(is.null(IMpath),"convert",file.path(IMpath,"convert"))
-    for (grain in grains){
+    for (i in seq_along(grains)){
+        grain <- grains[i]
         message(grain)
         igrain <- paste0(idir,grain)
-        ograin <- paste0(odir,grain)
-        if (!dir.exists(ograin)) dir.create(ograin)
         images <- list.files(igrain,pattern="\\.png$")
-        for (i in seq_along(images)){
-            ifname <- file.path(igrain,images[i])
-            ofname <- file.path(ograin,paste0("Stack-0",i-1,".jpg"))
-            cmd <- paste0(convert," -set colorspace Gray -quality 50 '",ifname,"' '",ofname,"'")
+        if (i<10){
+            gname <- paste0("Grain0",i)
+        } else {
+            gname <- paste0("Grain",i)
+        }
+        gdir <- paste0(odir,gname)
+        if (!dir.exists(gdir)) dir.create(gdir)
+        for (j in seq_along(images)){
+            ifname <- file.path(paste0(idir,"'",grain,"'"),images[j])
+            if (j<10){
+                stackname <- paste0("Stack-0",j)
+            } else {
+                stackname <- paste0("Stack-",j)
+            }
+            ofname <- file.path(gdir,paste0(stackname,".jpg"))
+            cmd <- paste0(convert," -set colorspace Gray -quality 50 ",ifname," ",ofname)
             system(cmd)
         }
         foo <- 1
